cc645ec908d1f2c5a88425da51b7da6c34e9a367
tomasmalleret <tomas.malleret@datamatrixcompany.com>
2024-07-31 13:40:33 -0300

    About view

--- src/App.tsx
+++ src/App.tsx
@@ -10,7 +10,7 @@ import Register from "./views/Register/Register";
 function App() {
   return (
     <>
-      <Contact />
+      <About />
     </>
   );
 }
--- src/components/templates/AboutTemplate/AboutTemplate.tsx
+++ src/components/templates/AboutTemplate/AboutTemplate.tsx
@@ -2,17 +2,24 @@ import React from "react";
 import "./AboutTemplate.css";
 import NavBar from "../../molecules/NavBar/NavBar";
 
-interface IAboutTemplate {
-  username: string;
-  email: string;
-  password: string;
-  city: string;
-}
-
 const AboutTemplate: React.FC = () => {
   return (
-    <div className="about-container">
+    <div className="about__container">
       <NavBar current_link="about" />
+      <div className="about__content">
+        <h1 className="about__title">¡Bienvenidos a Gangazo Inmobiliario!</h1>
+        <h2 className="about__subtitle">¿Que es Gangazo Inmobiliario?</h2>
+        <p className="about__text">
+          Gangazo Inmobiliario es tu portal de referencia para encontrar las
+          mejores ofertas inmobiliarias en Colombia. Utilizamos inteligencia
+          artificial y analisis de datos para identificar propiedades que, por
+          sus caracteristicas y precio, representat una oportunidad unica para
+          ti.
+        </p>
+        <a href="#" className="about__button">
+          Conoce mas
+        </a>
+      </div>
     </div>
   );
 };
